94ec8767dcf912f3ec6cdd645951bee2228a9e26
m the second <iammooo>
2020-03-06 11:31:03 +0100

    trying with java only, be carefull of inlining

---
+++ src/main/java/jbenchmarking/ArrayIteration.java
@@ -0,0 +1,8 @@
+package jbenchmarking;
+
+
+public class ArrayIteration{
+
+
+    
+}
---
+++ src/main/java/jbenchmarking/StringParty.java
@@ -0,0 +1,16 @@
+package jbenchmarking;
+
+
+/**
+ * Check datainputview .getUtf()
+ * Check   java.io DataInputStream .getUtf()
+ * 
+ * EfficientLazyString from git://git.eclipse.org/gitroot/m2t/org.eclipse.xpand.git  
+ * 
+ *  ideas from https://github.com/findify/flink-adt.git  , flink flow 2019
+ */
+
+public class StringParty{
+
+
+}
